8c1c749cc575fc25e7f0bb32d2c1bdccc3a0a493
Caio Lucas <caiolfsantoss@gmail.com>
2020-07-20 11:45:24 -0300

    botao-editar

--- src/componentes/ListaItem.js
+++ src/componentes/ListaItem.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 export default class ListaItem extends Component {
   render() {
-    const { titulo, DeletarItem, EditarItem } = this.props;
+    const { titulo, DeletarItem, Editar } = this.props;
 
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
         <h6>{titulo}</h6>
         <div className="lista-icone">
-          <span className="mx-2 text-success" onClick={EditarItem}>
+          <span className="mx-2 text-success" onClick={Editar}>
             <i className="fas fa-pen" />
           </span>
           <span className="mx-2 text-danger" onClick={DeletarItem}>
